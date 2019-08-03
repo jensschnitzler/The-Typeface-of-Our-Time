ef15dfb4ff9d8ce5b6616791e8624ed85425570d
jensschnitzler <jens.schnitzler@gmail.com>
2019-08-03 12:59:38 +0200

    update

--- assets/js/html-include.js
+++ assets/js/html-include.js
@@ -9,19 +9,21 @@ function includeHTML() {
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("w3-include-html");
     if (file) {
+      console.log('file: ' + file);
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
-          if (this.status == 200) {
+          if (this.status == 200) { //success
             elmnt.innerHTML = this.responseText;
+            console.log('this.responseText: ' + this.responseText);
+            filterContent(); // custom function
+            figureItOut();  // custom function from figure.js
           }
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
           /* Remove the attribute, and call this function once more: */
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
-          filterContent(); // custom function
-          figureItOut();  // custom function from figure.js
         }
       }
       xhttp.open("GET", file, true);
@@ -41,11 +43,11 @@ function filterContent() {
     if (elmnt.hasAttribute("data-filter")) {
       filterVal = elmnt.getAttribute("data-filter");
       if (filterVal) {
-        //console.log( 'filterVal ' + filterVal );
+        console.log( 'filterVal ' + filterVal );
         sources = elmnt.getElementsByClassName( String(filterVal) );
-        //console.log( 'source.length ' + sources.length );
+        console.log( 'source.length ' + sources.length );
         source = sources[0].innerHTML;
-        //console.log( 'source ' + source );
+        console.log( 'source ' + source );
         elmnt.innerHTML = source;
         //Object.assign(clone, source);
         //elmnt.innerHTML = clone;
