48c131a145a22ba6c8e20ca3df0a80153dfa54a3
jensschnitzler <jens.schnitzler@gmail.com>
2020-03-14 10:51:12 +0100

    main update

--- assets/js/paragraph-counter.js
+++ assets/js/paragraph-counter.js
@@ -5,7 +5,7 @@ function paragraphCounter(){
   var elements = $('body').find('p');
 
   elements.each(function(i){
-    console.log('i: ' + i);
+    //console.log('i: ' + i);
     var count = i + 1;
 
     var myCountBox = $(this).children('div.count');
