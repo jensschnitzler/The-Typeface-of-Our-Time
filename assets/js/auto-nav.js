f57c4d6283f62fe91bde7bd00c3550956c81ca37
jensschnitzler <jens.schnitzler@gmail.com>
2020-06-03 12:13:27 +0200

    update

--- assets/js/auto-nav.js
+++ assets/js/auto-nav.js
@@ -6,10 +6,12 @@ function autoNav(){
   container.html('');
 
   var main = $('body').find('main');
-  var headlines = main.find('h1,h2,h3,h4,h5,h6');
+  var headlines = main.find('h1, h2, h3, h4, h5, h6');
 
   headlines.each(function(i){
-    //console.log('i: ' + i);
+
+    console.log('i: ' + i);
+    
     var hCount = i + 1;
     var hType = $(this).prop('nodeName');
     var hContent = $(this).text();
@@ -18,6 +20,7 @@ function autoNav(){
     var navItem = '<a>' + hContent + '</a>';
 
     container.append(navItem);
+
     navItem.addClass('navItem');
     navItem.attr('data-count',hCount);
     navItem.attr('data-type',hType);
